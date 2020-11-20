9429b7738026ee8fcb3177b37d268b19215b83c9
glicerico <suarezandres@gmail.com>
2020-11-19 20:55:00 -0600

    Test URL getter code

--- ADAM/ADAM/spiders/parse_urls.py
+++ ADAM/ADAM/spiders/parse_urls.py
@@ -1,7 +1,22 @@
 import glob
+import xml.etree.cElementTree as ET
 
-my_path = "/home/andres/repositories/MedQuAD/10_MPlus_ADAM_QA/"
-extension = "*.xml"
-xml_list = [f for f in glob.glob(my_path + extension)]
 
-print(xml_list)
+def get_files(path):
+    for xml_file in glob.glob(path):
+        print(xml_file)
+        parseXML(xml_file)
+
+
+def parseXML(file_name):
+    # Parse XML with ElementTree
+    tree = ET.ElementTree(file=file_name)
+    root = tree.getroot()
+    print("tag=%s, url=%s" % (root.tag, root.attrib['url']))
+    print(type(root.attrib['url']))
+
+
+if __name__ == "__main__":
+    my_path = "/home/andres/repositories/MedQuAD/10_MPlus_ADAM_QA/"
+    extension = "*.xml"
+    get_files(my_path + extension)
