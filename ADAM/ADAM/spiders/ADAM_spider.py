b0e22722c986c4e9239dc92d96b730d2bdb6eb09
glicerico <suarezandres@gmail.com>
2020-11-19 19:56:34 -0600

    Basic test with medline website

--- ADAM/ADAM/spiders/ADAM_spider.py
+++ ADAM/ADAM/spiders/ADAM_spider.py
@@ -6,12 +6,10 @@ class ADAMSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://github.com/abachaa/MedQuAD/tree/master/10_MPlus_ADAM_QA'
+            'https://medlineplus.gov/ency/article/000321.htm'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for row in response.css('div.flex-auto.min-width-0.col-md-2.mr-3 a.js-navigation-open.link-gray-dark'):
-            print(row.attrib['href'] + '\n\n\n\n')
-            yield row.attrib['href']
+        yield {'test' : response.css('title::text').get()}
